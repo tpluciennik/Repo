a52bf3bfa3031bab0cfbb8a23dd48336b653288e
Tomasz Pluciennik <t.pluciennik@wtk.pl>
2015-07-21 13:02:46 +0200

    Pierwszy commit

---
+++ abc.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello World</title>
+    </head>
+    <body>
+    	<div>
+        <h1 style="color: red; margin-top: 100px; margin-left: 80px;">Siema</h1>
+        <input type="button" style="margin-left: 80px;" value="Kliknij"/>
+        </div>
+        <div style="z-index: -1; position: absolute; overflow: hidden; top: 30px; width: 100%;">
+        <video style="width: 100%" autoplay="autoplay" loop>
+	      <source type="video/mp4" src="news_sys/video/20357.mp4" />
+	    </video>
+	    </div>
+    </body>
+</html>
